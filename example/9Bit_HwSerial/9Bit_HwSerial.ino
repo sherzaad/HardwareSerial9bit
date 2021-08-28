2020b36e45b65c3da194afc19fe62a8becc6ba7e
Sherzaad <57634578+sherzaad@users.noreply.github.com>
2021-08-28 08:40:57 +0100

    Update 9Bit_HwSerial.ino

--- example/9Bit_HwSerial/9Bit_HwSerial.ino
+++ example/9Bit_HwSerial/9Bit_HwSerial.ino
@@ -1,47 +1,26 @@
-#include <SoftwareSerial.h>
 #include <HardwareSerial9bit.h>
-
-#define BAUD 9600
-
-SoftwareSerial mySerial(3, 4); // RX, TX
-
-uint8_t n=0;
 
 void setup() {
   
-  // Open serial communications and wait for port to open:
-  Serial.begin(BAUD);
+  // Open serial communications and wait for port to open. (COM Port for PC Serial Monitor)
+  Serial.begin(115200);
   while (!Serial) {
     ; // wait for serial port to connect. Needed for native USB port only
   }
 
-  //Open seial communication to recieve 9bit data
+  //Open seial communication to recieve 9bit data (using Serial1 COM port for 9 bit Serial communication) 
   Serial91.begin(9600, SERIAL_9N1);
-  // set the data rate for the SoftwareSerial port
-  mySerial.begin(BAUD);
 
   Serial.println("READY!");
 }
 
 void loop() {
-uint16_t indata=0;
+uint16_t indata;
   
-  Serial.print("SS TX = ");
-  if(n<=15) Serial.print("0x0");
-  else Serial.print("0x");
-  Serial.print(n,HEX);
-  Serial.print(", ");
-  
-  mySerial.write(n);
-  
-  delay(500);
-
-  n = (n+1)%256;
-  
-  Serial.print(Serial91.available());
-  Serial.print(", S1 RX = ");
-  while(Serial91.available()>0){
-    Serial.print((int)Serial91.read(),HEX);   
+  if(Serial91.available()){
+    indata = (int)Serial91.read(); //read 9bit Serial Data
+    Serial.print("S1 RX = ");
+    Serial.print(indata,HEX); //print 9bit Serial Data to Serial Monitor
   }
   Serial.println("");
 }
