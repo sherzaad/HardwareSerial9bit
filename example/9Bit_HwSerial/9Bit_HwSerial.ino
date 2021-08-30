a3672e158db20a7a66344e18a77d515753b9684f
Sherzaad <57634578+sherzaad@users.noreply.github.com>
2021-08-30 21:51:04 +0100

    Update 9Bit_HwSerial.ino

--- example/9Bit_HwSerial/9Bit_HwSerial.ino
+++ example/9Bit_HwSerial/9Bit_HwSerial.ino
@@ -3,15 +3,15 @@
 void setup() {
   
   // Open serial communications and wait for port to open. (COM Port for PC Serial Monitor)
-  Serial.begin(115200);
-  while (!Serial) {
+  Serial90.begin(115200);
+  while (!Serial90) {
     ; // wait for serial port to connect. Needed for native USB port only
   }
 
   //Open seial communication to recieve 9bit data (using Serial1 COM port for 9 bit Serial communication) 
   Serial91.begin(9600, SERIAL_9N1);
 
-  Serial.println("READY!");
+  Serial90.println("READY!");
 }
 
 void loop() {
@@ -19,10 +19,10 @@ uint16_t indata;
   
   if(Serial91.available()){
     indata = (int)Serial91.read(); //read 9bit Serial Data
-    Serial.print("S1 RX = ");
-    Serial.print(indata,HEX); //print 9bit Serial Data to Serial Monitor
+    Serial90.print("S1 RX = ");
+    Serial90.print(indata,HEX); //print 9bit Serial Data to Serial Monitor
   }
-  Serial.println("");
+  Serial90.println("");
   
   delay(500); //arbitrary delay
 }
